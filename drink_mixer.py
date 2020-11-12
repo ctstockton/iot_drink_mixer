d396496614d7b48421edd07f945480402173a6cf
Corey Stockton <coreystockton4c@gmail.com>
2020-11-12 14:10:38 -0500

    linked webhook to request manager
    uses a shared queue for communication

--- drink_mixer.py
+++ drink_mixer.py
@@ -1,15 +1,28 @@
 # This is the drink mixer object
+from queue import Queue
+from threading import Thread
+from request_manager import request_manager
+from webhook_service import webhook_service
 
 class drink_mixer(object):
     """
     This is the drink mixer object, managing local and remote requests.
     """
     def __init__(self):
-        pass
+        self.q = Queue()
+        self.webhook_service = webhook_service(self.q)
+        self.webhook_service_thread = Thread(target=self.webhook_service.run)
+        #self.GUI_service = Thread()#target=, args=(self.q, ))
+
+        self.request_manager = request_manager(self.q)
 
     def run(self):
-        while True:
-            pass
+        # start threads
+        self.webhook_service_thread.start()
+        #self.GUI_service.start()
+
+        # start manager of the main thread
+        self.request_manager.run()
 
 if __name__ == "__main__":
     drinkMixer = drink_mixer()
